fdb81dd7879b67e9e6b462b8c578071a833647a9
Enock Tangus <tanguskipngeno@gmail.com>
2025-04-08 16:15:08 +0300

    added the two transactions' rows

---
+++ src/components/TransactionRow.jsx
@@ -0,0 +1,11 @@
+export default function TransactionRow({id, type, amount}) {
+  return (
+    <>
+      <tr>
+        <td>{id}</td>
+        <td>{type}</td>
+        <td>{amount}</td>
+      </tr>
+    </>
+  );
+}
--- src/components/TransactionsTable.jsx
+++ src/components/TransactionsTable.jsx
@@ -1,16 +1,17 @@
-export default function TransactionsTable(){
-
-    return(
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-        </table>
-    )
-
+import TransactionRow from './TransactionRow'
+export default function TransactionsTable() {
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <TransactionRow id="1" type="Withdraw" amount="5,000"/> 
+      <TransactionRow id="2" type="Deposit" amount="3,000,000"/> 
+    </table>
+  );
 }
