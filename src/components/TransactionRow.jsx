05b8989bb16f6bcb59e2d60e7671a57f128f4b21
Enock Tangus <tanguskipngeno@gmail.com>
2025-04-08 16:19:29 +0300

    remove empty fragment

--- src/components/TransactionRow.jsx
+++ src/components/TransactionRow.jsx
@@ -1,11 +1,9 @@
 export default function TransactionRow({id, type, amount}) {
   return (
-    <>
       <tr>
         <td>{id}</td>
         <td>{type}</td>
         <td>{amount}</td>
       </tr>
-    </>
   );
 }
